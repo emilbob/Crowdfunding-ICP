4721c30918ce4cbac43abaea85e1f7a303b19d59
emil bob <emilbob03@gmail.com>
2024-02-07 17:43:49 +0100

    fixed methods

--- src/index.ts
+++ src/index.ts
@@ -12,20 +12,36 @@ import {
   nat64,
   ic,
   Variant,
+  Principal,
 } from "azle";
 import { v4 as uuidv4 } from "uuid";
 
-const Campaign = Record({
+const CampaignPayload = Record({
   title: text,
   description: text,
   goalAmount: nat64,
 });
 
+const Campaign = Record({
+  id: text,
+  title: text,
+  description: text,
+  goalAmount: nat64,
+  currentAmount: nat64,
+  startDate: nat64,
+  endDate: nat64,
+  owner: Principal,
+});
+
+type Campaign = typeof Campaign.tsType;
+
 const Contribution = Record({
-  contributor: text,
+  contributor: Principal,
   amount: nat64,
   timestamp: nat64,
 });
+
+type Contribution = typeof Contribution.tsType;
 
 const Error = Variant({
   CampaignNotFound: text,
@@ -35,42 +51,44 @@ const Error = Variant({
   StorageError: text,
 });
 
-const campaignsStorage = StableBTreeMap(text, Campaign, 0);
-const contributionsStorage = StableBTreeMap(text, Vec(Contribution), 0);
+const campaignsStorage = StableBTreeMap<text, Campaign>(0);
+const contributionsStorage = StableBTreeMap<text, Vec<Contribution>>(1);
 
 export default Canister({
   //Update methods
 
   // Create campaign
-  createCampaign: update([Campaign], Result(text, Error), (campaignPayload) => {
-    if (
-      !campaignPayload.title ||
-      !campaignPayload.description ||
-      campaignPayload.goalAmount <= 0n
-    ) {
-      return Err({
-        ValidationError:
-          "Invalid input data: Ensure title, description are present and goal amount is positive.",
-      });
+  createCampaign: update(
+    [CampaignPayload],
+    Result(text, Error),
+    (campaignPayload) => {
+      if (
+        !campaignPayload.title ||
+        !campaignPayload.description ||
+        campaignPayload.goalAmount <= 0n
+      ) {
+        return Err({
+          ValidationError:
+            "Invalid input data: Ensure title, description are present and goal amount is positive.",
+        });
+      }
+      const campaignId = uuidv4();
+      const twentyFourHoursInNanoseconds = 24n * 60n * 60n * 1_000_000_000n;
+      const endDate = ic.time() + twentyFourHoursInNanoseconds;
+      const newCampaign = {
+        id: campaignId,
+        title: campaignPayload.title,
+        description: campaignPayload.description,
+        goalAmount: campaignPayload.goalAmount,
+        currentAmount: 0n,
+        owner: ic.caller(),
+        startDate: ic.time(),
+        endDate: endDate,
+      };
+      campaignsStorage.insert(campaignId, newCampaign);
+      return Ok(campaignId);
     }
-
-    const campaignId = uuidv4();
-
-    const twentyFourHoursInNanoseconds = 24n * 60n * 60n * 1_000_000_000n;
-    const endDate = ic.time() + twentyFourHoursInNanoseconds;
-
-    const newCampaign = {
-      ...campaignPayload,
-      id: campaignId,
-      currentAmount: 0n,
-      owner: ic.caller(),
-      startDate: ic.time(),
-      endDate: endDate,
-    };
-
-    campaignsStorage.insert(campaignId, newCampaign);
-    return Ok(campaignId);
-  }),
+  ),
 
   // Contribute to campaign
   contribute: update(
@@ -83,19 +101,15 @@ export default Canister({
           CampaignNotFound: `Campaign with id=${campaignId} not found`,
         });
       }
-
       let campaign = campaignOpt.Some;
-
       if (ic.time() > campaign.endDate) {
         return Err({ ValidationError: "This campaign has already ended." });
       }
-
       if (campaign.currentAmount >= campaign.goalAmount) {
         return Err({
           ContributionError: `Campaign with id=${campaignId} has successfully reached its funding goal. No further contributions are needed.`,
         });
       }
-
       // Prevent contribution from exceeding goal amount
       const newAmount = campaign.currentAmount + amount;
       if (newAmount > campaign.goalAmount) {
@@ -103,20 +117,16 @@ export default Canister({
           ContributionError: `This contribution would exceed the campaign's funding goal. Please adjust the contribution amount.`,
         });
       }
-
       campaign.currentAmount = newAmount;
       campaignsStorage.insert(campaignId, campaign);
-
       const contribution = {
         contributor: ic.caller(),
         amount,
         timestamp: ic.time(),
       };
-
       let contributions = contributionsStorage.get(campaignId).Some || [];
       contributions.push(contribution);
       contributionsStorage.insert(campaignId, contributions);
-
       return Ok(
         `Contributed ${amount} for the campaign with id: ${campaignId}`
       );
@@ -137,13 +147,11 @@ export default Canister({
         AuthorizationError: "Only the campaign owner can withdraw funds",
       });
     }
-
     if (campaign.currentAmount < campaign.goalAmount) {
       return Err({
         AuthorizationError: "Campaign has not reached its funding goal",
       });
     }
-
     return Ok(
       `Successfully withdrawn goal amount from the ${campaignId} camapign`
     );
@@ -157,9 +165,7 @@ export default Canister({
         CampaignNotFound: `Campaign with id=${campaignId} not found`,
       });
     }
-
     const campaign = campaignOpt.Some;
-
     // Check if caller is the owner and goal is reached
     if (ic.caller() !== campaign.owner) {
       return Err({
@@ -169,28 +175,13 @@ export default Canister({
     if (campaign.currentAmount < campaign.goalAmount) {
       return Err({ ValidationError: "Campaign goal has not been reached yet" });
     }
-
-    campaignsStorage.delete(campaignId);
-
+    campaignsStorage.remove(campaignId);
     return Ok(`Campaign ${campaignId} deleted successfully`);
   }),
 
   //   //Query methods
 
-  //Get campaigns
-  getCampaigns: query([], Result(Vec(Campaign), Error), () => {
-    try {
-      const campaigns = campaignsStorage.values();
-      return Ok(campaigns);
-    } catch (error) {
-      // Handle any unexpected errors
-      return Err({
-        StorageError: "Failed to retrieve campaigns from storage.",
-      });
-    }
-  }),
-
-  //Get one campaign
+  //Get one campaign by id
   getCampaign: query([text], Result(Campaign, Error), (id) => {
     const campaignOpt = campaignsStorage.get(id);
     if ("None" in campaignOpt) {
@@ -199,7 +190,17 @@ export default Canister({
     return Ok(campaignOpt.Some);
   }),
 
-  //List contributors
+  //Get campaigns
+  // Get campaigns
+  getCampaigns: query([], Result(Vec(Campaign), Error), () => {
+    const campaigns: Campaign[] = [];
+    for (const campaign of campaignsStorage.values()) {
+      campaigns.push(campaign);
+    }
+    return Ok(campaigns);
+  }),
+
+  //List contributions
   listContributions: query(
     [text],
     Result(Vec(Contribution), Error),
